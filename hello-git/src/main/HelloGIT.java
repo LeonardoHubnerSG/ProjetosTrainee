c413783ebc61cfaa6f451bbf65cdbd0f7f2aa48a
Leonardo Hubner <leonardo.hubner@sgsistemas.com.br>
2022-09-02 13:32:47 -0300

    chamando o metodo imprimeMsg2() no main

--- hello-git/src/main/HelloGIT.java
+++ hello-git/src/main/HelloGIT.java
@@ -4,7 +4,7 @@ public class HelloGIT {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
         imprimeMsg();
-
+        imprimeMsg2();
     }
 
     public static void imprimeMsg(){
