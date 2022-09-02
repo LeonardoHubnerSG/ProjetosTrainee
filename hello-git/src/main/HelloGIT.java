7abd858dc48ac5eb6e93051008499a29463b3746
Leonardo Hubner <leonardo.hubner@sgsistemas.com.br>
2022-09-02 10:05:26 -0300

    método novo criado (dentro da branch OS-123456)

--- hello-git/src/main/HelloGIT.java
+++ hello-git/src/main/HelloGIT.java
@@ -3,5 +3,11 @@ package main;
 public class HelloGIT {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
+        imprimeMsg();
+
+    }
+
+    public static void imprimeMsg(){
+        System.out.println("Meu primerio commit na branch OS-123456");
     }
 }
