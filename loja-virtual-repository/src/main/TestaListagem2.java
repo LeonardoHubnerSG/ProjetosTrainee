66768e84ae761ab53403651bbb5c4ea091632f31
Leonardo Hubner <leonardo.hubner@sgsistemas.com.br>
2022-09-15 08:17:35 -0300

    Loja virtual - Estudando Banco de dados em Java e JDBC

---
+++ loja-virtual-repository/src/main/TestaListagem2.java
@@ -0,0 +1,27 @@
+package main;
+
+import conexao.ConnectionFactory;
+import model.Produto;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TestaListagem2 {
+    public static void main(String[] args) throws SQLException {
+        PreparedStatement preparedStatement =
+                ConnectionFactory.obterConexao().prepareStatement("SELECT id,nome,descricao FROM produto");
+        preparedStatement.execute();
+
+        ResultSet resultSet = preparedStatement.getResultSet();
+
+        Produto produto = new Produto();
+
+        while(resultSet.next()){
+            produto.setId(resultSet.getInt(1));
+            produto.setNome(resultSet.getString(2));
+            produto.setDescricao(resultSet.getString(3));
+            System.out.println(produto.toString());
+        }
+    }
+}
