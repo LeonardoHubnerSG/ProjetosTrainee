d8193148122b7ec1cca3cdcc1922937e7b970430
Leonardo Hubner <leonardo.hubner@sgsistemas.com.br>
2022-09-20 18:01:32 -0300

    Projetos do curso de JPA

---
+++ jpa-modelos-complexos-curso-alura/src/main/java/br/com/alura/loja/dao/ClienteDao.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja.dao;
+
+import br.com.alura.loja.modelo.Cliente;
+
+import javax.persistence.EntityManager;
+
+public class ClienteDao {
+    private EntityManager em;
+
+    public ClienteDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void cadastrar(Cliente cliente) {
+this.em.persist(cliente);
+    }
+
+    public Cliente buscarPorId(long l) {
+        return em.find(Cliente.class, l);
+    }
+}
