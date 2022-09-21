5762a8ab8771ed4c5332f86f5365a033fc625a55
Leonardo Hubner <leonardo.hubner@sgsistemas.com.br>
2022-09-21 18:02:21 -0300

    Continuar os testes com o JOINED e SINGLE_TABLE

---
+++ sg-erp/src/main/java/br/com/sgsistemas/model/Paciente.java
@@ -0,0 +1,19 @@
+package br.com.sgsistemas.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Paciente extends Pessoa {
+    private String cpf;
+
+    public Paciente() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
