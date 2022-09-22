bbc33a63499c4c9f529eecc287d27a7709a6ba8d
Leonardo Hubner <leonardo.hubner@sgsistemas.com.br>
2022-09-22 00:22:33 -0300

    Classe Pessoa com o InheritanceType.SINGLE_TABLE e InheritanceType.JOINED funcionando quando usados

--- sg-erp/src/main/java/br/com/sgsistemas/dao/MedicoDAO.java
+++ sg-erp/src/main/java/br/com/sgsistemas/dao/MedicoDAO.java
@@ -1,4 +1,26 @@
 package br.com.sgsistemas.dao;
 
+import br.com.sgsistemas.model.Medico;
+
+import javax.persistence.EntityManager;
+
 public class MedicoDAO {
+    private EntityManager em;
+
+    public MedicoDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void cadastrar(Medico medico){
+        this.em.persist(medico);
+    }
+
+    public void atualizar(Medico medico){
+        this.em.merge(medico);
+    }
+
+    public void remover(Medico medico){
+        medico = em.merge(medico);
+        this.em.remove(medico);
+    }
 }
--- sg-erp/src/main/java/br/com/sgsistemas/dao/PacienteDAO.java
+++ sg-erp/src/main/java/br/com/sgsistemas/dao/PacienteDAO.java
@@ -1,4 +1,26 @@
 package br.com.sgsistemas.dao;
 
+import br.com.sgsistemas.model.Paciente;
+
+import javax.persistence.EntityManager;
+
 public class PacienteDAO {
+    private EntityManager em;
+
+    public PacienteDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void cadastrar(Paciente paciente){
+        this.em.persist(paciente);
+    }
+
+    public void atualizar(Paciente paciente){
+        this.em.merge(paciente);
+    }
+
+    public void remover(Paciente paciente){
+        paciente = em.merge(paciente);
+        this.em.remove(paciente);
+    }
 }
